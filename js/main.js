38bd75db8750b96bc1551aba7c7f9c8122f6d5bf
Martin Høite <mh@komunikado.dk>
2019-09-10 05:47:08 +0200

    Added API objects

--- js/main.js
+++ js/main.js
@@ -28,7 +28,33 @@ $(document).ready(function () {
 
 // #region Objects
 
+function Task(Id, Title, UserId, KanbanColumnId, Description, Estimate, Deadline, Priority, Order, Timestamp) {
+    this.Id = Id;
+    this.Title = Title;
+    this.UserId = UserId;
+    this.KanbanColumnId = KanbanColumnId;
+    this.Description = Description;
+    this.Estimate = Estimate;
+    this.Deadline = Deadline;
+    this.Priority = Priority;
+    this.Order = Order;
+    this.Timestamp = Timestamp;
+}
 
+function KanbanColumn(Id, Name, Color, Order, Timestamp) {
+    this.Id = Id;
+    this.Name = Name;
+    this.Color = Color;
+    this.Order = Order;
+    this.Timestamp = Timestamp;
+}
+
+function User(Id, Username, Image, Timestamp) {
+    this.Id = Id;
+    this.Username = Username;
+    this.Image = Image;
+    this.Timestamp = Timestamp;
+}
 
 // #endregion Objects
 
@@ -53,7 +79,7 @@ function confirmCloseModal(modalId) {
 }
 
 
-function createNewTask(){
+function createNewTask() {
 
 }
 

c004d7262f6d45951bd7d6137fbfdb39c595e8b9
Martin Høite <mh@komunikado.dk>
2019-09-10 05:40:57 +0200

    Setup layout, buttons and dragging functionality Setup create task modal

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
     $('.fixed-action-btn').floatingActionButton();
     $('.tooltipped').tooltip();
     $('.modal').modal();
+    $('select').formSelect();
+    $('.datepicker').datepicker();
+    $('.timepicker').timepicker();
 
     $('.tasks__row').sortable({
         group: 'row',
@@ -12,10 +15,46 @@ $(document).ready(function () {
     });
 
     $('.tasks__column').sortable({
+        swapThreshold: 1,
+        invertSwap: true,
         group: 'column',
         animation: 250,
         ghostClass: 'grey',
         handle: ".column-drag-handle"
     });
 
+    // sendNotification('Hello world!', 1000, 'green');
 });
+
+// #region Objects
+
+
+
+// #endregion Objects
+
+// #region Functions
+
+function sendNotification(content, _displayLength, _classes) {
+    M.toast({
+        html: content,
+        displayLength: _displayLength,
+        classes: _classes
+    });
+}
+
+function confirmCloseModal(modalId) {
+    let modalEl = $(modalId);
+    var modalInstance = M.Modal.getInstance(modalEl);
+
+    var confirmed = confirm('Are you sure you wish to cancel?');
+    if (confirmed == true) {
+        modalInstance.close();
+    }
+}
+
+
+function createNewTask(){
+
+}
+
+// #endregion Functions
